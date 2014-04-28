50a3b007d4f17db0512c4a09e3b6463071dcd9bc
Filip Bober <filip.bober@gmail.com>
2014-04-28 12:40:58 +0200

    Cleaned and updated ActionSetExt.hpp: - Removed unused defines - Removed commented ActionSet single-line-methods code - Changed old S_LSHIFT TO S_LSHIFT_LEGACY - New S_LSHIFT works on real BITSET_SIZE (and S_SINGLE_BIT as works as well)

--- StarcraftBuildOrderSearch/Source/starcraftsearch/ActionSetExt.hpp
+++ StarcraftBuildOrderSearch/Source/starcraftsearch/ActionSetExt.hpp
@@ -7,37 +7,18 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 //
-// BitSet
+// BitSetExt
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-//#ifdef WIN32
-//#include <intrin.h>
-//#pragma intrinsic(__ll_lshift)
-//#pragma intrinsic(__ll_rshift)
 #define __lz(a) countLeadingZeros(a)
 #define __tz(a) countTrailingZeros(a)
-//#define __ONE 1ull
-//#define __ZERO 0ull
-//#define __LSHIFT64(VAL, N) __ll_lshift(VAL, N)
-//#define __RSHIFT64(VAL, N) __ll_rshift(VAL, N)
-//#else
-//#define __lz(a) __builtin_clzll(a)
-//#define __tz(a) __builtin_ctzll(a)
-//#define __ONE 1LLU
-//#define __ZERO 0LLU
-//#define __LSHIFT64(VAL, N) VAL << N
-//#define __RSHIFT64(VAL, N) VAL >> N
-//#endif
-//
-//#define LBIT(N) (__LSHIFT64(__ONE, (63-N)))
-//#define RBIT(N) (__LSHIFT64(__ONE, N))
-//#define SINGLE_BIT(N) RBIT(N)
 
 
 #define S_ONE std::bitset<BITSET_SIZE>(1)
 #define S_ZERO std::bitset<BITSET_SIZE>(0)
-#define S_LSHIFT(VAL, N) ((VAL) << ((N/2)-1))
+#define S_LSHIFT_LEGACY(VAL, N) ((VAL) << ((N/2)-1))			// incorrect code, compatibile with 32 int and defautl ActionSet
+#define S_LSHIFT(VAL, N) ((VAL) << ((N-1)))
 #define S_SINGLE_BIT(N) (S_LSHIFT(S_ONE, N))
 
 #define __ONE S_ONE
@@ -45,34 +26,17 @@
 
 //#define REVERSE_ACTION_ITERATOR
 
-// For testing purposes
-//namespace ActionSetTestUnitTests
-//{
-//	class ActionSetExtTest;
-//}
-// eof testing
-
 namespace BuildOrderSearch
 {
 	typedef unsigned char Action;
 	const unsigned int BITSET_SIZE = 64;
 
-	//std::bitset<BITSET_SIZE> S_ONE = std::bitset<BITSET_SIZE>(1);
-	//std::bitset<BITSET_SIZE> S_ZERO = std::bitset<BITSET_SIZE>(0);
-
 	class BitSetExt
 	{
-		//friend class ActionSetTestUnitTests::ActionSetExtTest;
-		//unsigned long long set;							// 64 bit unsigned int to represent set
-
 		std::bitset<BITSET_SIZE> setExt;
-		//std::bitset<BITSET_SIZE> S_ONE = __ONE;
-		//std::bitset<BITSET_SIZE> S_ONE = std::bitset<BITSET_SIZE>(1);
 
-		//setExt(std::bitset<BITSET_SIZE>(1) << (BITSET_SIZE - 1))
 	public:
 		BitSetExt() : setExt(0) {}							// default constructor sets to zero
-		//BitSetExt(long long unsigned s) : setExt(s) {}		// constructor which takes a uint64
 		BitSetExt(std::bitset<BITSET_SIZE> s) : setExt(s) {}
 
 		// pops the next action (bit from the right)
@@ -88,7 +52,6 @@ namespace BuildOrderSearch
 
 			// set that bit to a zero
 			subtract(nextAction);
-			//set = setExt.to_ulong();
 
 			return (Action)nextAction;
 		}
@@ -107,32 +70,8 @@ namespace BuildOrderSearch
 			return nextAction;
 		}
 
-		// old (but working for bitset <mostly>)
-		//int  		operator [] 	(const int bit) 	const { return set & SINGLE_BIT(bit) ? 1 : 0; }	// get the bit at i
-		//BitSetExt 		operator +  	(const int bit) 	const { return BitSetExt(set | SINGLE_BIT(bit)); }	// member addition
-		//BitSetExt 		operator -  	(const int bit) 	const { return BitSetExt(set & ~SINGLE_BIT(bit)); }	// member subtraction
-		//BitSetExt 		operator |  	(const BitSetExt & a) 	const { return BitSetExt(set | a.set); }				// set union
-		//BitSetExt 		operator +  	(const BitSetExt & a)	const { return BitSetExt(set | a.set); }				// set union
-		//BitSetExt 		operator -  	(const BitSetExt & a)	const { return BitSetExt(set & ~a.set); }			// set subtraction
-		//BitSetExt 		operator &  	(const BitSetExt & a)	const { return BitSetExt(set & a.set); }				// set intersection
-		//BitSetExt 		operator ~  	()					const { return BitSetExt(~set); }					// set negation
-
-		// old (unsigned long long)
-		//bool 		isEmpty() 					const { return set == __ZERO; }					// the set is all zeros
-		//bool 		contains(const BitSetExt a) 	const { return (set & a.set) == a.set; }			// completely contain another set
-		//bool		contains(const int bit) 	const { return (set & SINGLE_BIT(bit)) != __ZERO; }// contains a bit set to 1
-		//bool		containsAny(const BitSetExt a)	const { return (set & ~a.set) != set; }			// does set contain any of
-		//bool		containsNone(const BitSetExt a)	const { return (set & ~a.set) == set; }			// does set contain none of 
-		//bool 		isSubsetOf(const BitSetExt a) 	const { return (a.set & set) == set; }			// is a subset of another set
-		//int 		getBit(const int bit)		const { return (set & SINGLE_BIT(bit)) ? 1 : 0; }	// identical to contains
-		//void 		add(const int bit) { set |= SINGLE_BIT(bit); }					// sets a bit to 1
-		//void 		add(const BitSetExt a) { set |= a.set; }							// sets all of input set to 1
-		//void		subtract(const int bit) { set &= ~SINGLE_BIT(bit); }				// set bit to zero
-		//void 		subtract(const BitSetExt a) { set &= ~a.set; }							// sets all of input set to 0
-
-		// new 
 		int  		operator [] 	(const int bit) 	const { return setExt[bit]; }	// get the bit at i
-		BitSetExt 		operator +  	(const int bit) 	const { return BitSetExt(setExt | S_SINGLE_BIT(bit)); }	// member addition // setExt | std::bitset<BITSET_SIZE>(bit)  
+		BitSetExt 		operator +  	(const int bit) 	const { return BitSetExt(setExt | S_SINGLE_BIT(bit)); }	// member addition // setExt | std::bitset<BITSET_SIZ(bit  
 		BitSetExt 		operator -  	(const int bit) 	const { return BitSetExt(setExt & ~S_SINGLE_BIT(bit)); }	// member subtraction
 		BitSetExt 		operator |  	(const BitSetExt & a) 	const { return BitSetExt(setExt | a.setExt); }				// set union
 		BitSetExt 		operator +  	(const BitSetExt & a)	const { return BitSetExt(setExt | a.setExt); }				// set union
@@ -182,7 +121,7 @@ namespace BuildOrderSearch
 			return zeros;
 		}
 
-		int countLeadingZeros(std::bitset<BITSET_SIZE> s) const
+		int countLeadingZerosLegacy(std::bitset<BITSET_SIZE> s) const
 		{
 			//int zeros = 0;
 			//unsigned long long __L_ONE = __LSHIFT64(__ONE, 63);
@@ -196,7 +135,9 @@ namespace BuildOrderSearch
 			//return zeros;
 
 			std::bitset<BITSET_SIZE> sBs = std::bitset<BITSET_SIZE>(s);		
-			std::bitset<BITSET_SIZE> sLeading = (S_ONE << ((BITSET_SIZE/2) - 1));		// note: BITSET_SIZE / 2
+			//std::bitset<BITSET_SIZE> sLeading = (S_ONE << ((BITSET_SIZE/2) - 1));		// note: BITSET_SIZE / 2
+			//std::bitset<BITSET_SIZE> sLeading = S_LSHIFT(S_ONE, (BITSET_SIZE/2 ));
+			std::bitset<BITSET_SIZE> sLeading = S_LSHIFT_LEGACY(S_ONE, (BITSET_SIZE));
 
 			int zeros = 0;
 
@@ -215,7 +156,7 @@ namespace BuildOrderSearch
 			return zeros;
 		}
 
-		int countAllLeadingZeros(std::bitset<BITSET_SIZE> s) const
+		int countLeadingZeros(std::bitset<BITSET_SIZE> s) const
 		{
 			//int zeros = 0;
 			//unsigned long long __L_ONE = __LSHIFT64(__ONE, 63);
@@ -229,8 +170,8 @@ namespace BuildOrderSearch
 			//return zeros;
 
 			std::bitset<BITSET_SIZE> sBs = std::bitset<BITSET_SIZE>(s);
-			//std::bitset<BITSET_SIZE> sLeading = (S_ONE << (BITSET_SIZE - 1));
-			std::bitset<BITSET_SIZE> sLeading = S_LSHIFT(S_ONE, (BITSET_SIZE - 1));
+			std::bitset<BITSET_SIZE> sLeading = (S_ONE << (BITSET_SIZE - 1));
+			//std::bitset<BITSET_SIZE> sLeading = S_LSHIFT(S_ONE, (BITSET_SIZE - 1));
 
 			int zeros = 0;
 
@@ -246,30 +187,7 @@ namespace BuildOrderSearch
 
 		int numActions() const
 		{
-			//BitSetExt t(set);
-			//int count(0);
-
-			//while (!t.isEmpty())
-			//{
-			//	t.popAction();
-			//	++count;
-			//}
-			//return count;
-
-			//BitSetExt t(set);
-			//int count(0);
-
-			//while (!t.isEmpty())
-			//{
-			//	t.popAction();
-			//	++count;
-			//}
-			//return count;
-
-
-			// correct, but commented for testing purposes
 			return setExt.count();
-
 		}
 
 		Action randomAction() const
@@ -301,6 +219,8 @@ namespace BuildOrderSearch
 			//printf("\n");
 			//for (int i = 0; i < 64; ++i) { printf("%d", (set & (__LSHIFT64(__ONE, (63 - i))) ? 1 : 0)); }
 			//printf("\n");
+
+			printf("Print is not implemented\n");
 		}
 	};
 
--- Tests/ActionSetExtTests.cpp
+++ Tests/ActionSetExtTests.cpp
@@ -21,11 +21,6 @@ namespace ActionSetTestUnitTests
 		
 
 	public:
-		//TEST_MODULE_INITIALIZE(ModuleStartup)
-		//{
-		//	Logger::WriteMessage("The test is starting");
-		//}
-
 		TEST_METHOD(TestMethod1)
 		{
 			// TODO: Your test code here
@@ -206,23 +201,6 @@ namespace ActionSetTestUnitTests
 
 				Assert::AreEqual(expectedValue, actualValue);
 			}
-
-			//for (int i = 0; i < MAX_DEFAULT_SET_SIZE; i++)
-			//{
-			//	actionSet = BuildOrderSearchDeprecated::ActionSet(i);
-			//	actionSetExt = ActionSetExt(i);
-			//	for (int s = 0; s < INT_MAX - 2; s++)
-			//	{
-			//		actionSet.add((int)s);
-			//		actionSetExt.add((int)s);
-
-
-			//		expectedValue = actionSet.popAction();
-			//		actualValue = actionSetExt.popAction();
-
-			//		Assert::AreEqual(expectedValue, actualValue);
-			//	}
-			//}
 		}
 
 		TEST_METHOD(subtractBit)
@@ -423,7 +401,8 @@ namespace ActionSetTestUnitTests
 				actionSetExt = ActionSetExt(i);
 
 				expectedValue = actionSet.countLeadingZeros(i);
-				actualValue = actionSetExt.countLeadingZeros(i);
+				//actualValue = actionSetExt.countLeadingZeros(i);
+				actualValue = actionSetExt.countLeadingZerosLegacy(i);
 
 				Assert::AreEqual(expectedValue, actualValue);
 			}
